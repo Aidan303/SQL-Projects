09c4e83fa81cb96d0d4647a9ab26637fb0d9666f
Aidan Hartman <aidan_hartman@yahoo.com>
2025-06-10 16:12:06 -0600

    Table reupload after excel cleaning

--- Project_Premier_League_Data_Analysis/sql_load/3_modify_tables.sql
+++ Project_Premier_League_Data_Analysis/sql_load/3_modify_tables.sql
@@ -1,9 +1,9 @@
 /*
-\copy prem.player_possession_stats FROM 'C:\Users\aidan\SQL Projects\csv_files\Prem CSV Files\player_possession_stats.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy prem.player_possession_stats FROM 'C:\Users\aidan\SQL Projects\csv_files\Prem CSV Files\player_possession_stats_cleaned.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy prem.player_salaries FROM 'C:\Users\aidan\SQL Projects\csv_files\Prem CSV Files\player_salaries.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy prem.player_salaries FROM 'C:\Users\aidan\SQL Projects\csv_files\Prem CSV Files\player_salaries_cleaned.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-\copy prem.player_stats FROM 'C:\Users\aidan\SQL Projects\csv_files\Prem CSV Files\player_stats.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
+\copy prem.player_stats FROM 'C:\Users\aidan\SQL Projects\csv_files\Prem CSV Files\player_stats_cleaned.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
 \copy prem.standings FROM 'C:\Users\aidan\SQL Projects\csv_files\Prem CSV Files\standings.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
